db347e8cb651a73b7609fbf98eefd1b0c8efc6c6
kbr <kbr@dev.java.net>
2004-02-28 01:31:06 +0000

    Removed handwritten native code for OpenAL binding and reenabled handwritten EAX binding in the build.
    git-svn-id: file:///home/mbien/NetBeansProjects/JOGAMP/joal-sync/git-svn/../svn-server-sync/joal/branches/gluegen-branch-1-0@93 03bf7f67-59de-4072-a415-9a990d468a3f

--- src/native/eaxbind.c
+++ src/native/eaxbind.c
@@ -33,7 +33,7 @@
 
 
 #include "eaxbind.h"
-#include "extal.h"
+#include "eax.h"
 #ifdef _WIN32
 const GUID DSPROPSETID_EAX20_ListenerProperties
 				= { 0x306a6a8, 0xb224, 0x11d2, { 0x99, 0xe5, 0x0, 0x0, 0xe8, 0xd8, 0xc7, 0x22 } };
