1c505633e784a5ec3878b152fe6890a03c700304
kbr <kbr@dev.java.net>
2007-04-08 16:41:06 +0000

    Added alcGetCaptureDeviceSpecifiers() based on feedback on javagaming.org forums
    git-svn-id: file:///home/mbien/NetBeansProjects/JOGAMP/joal-sync/git-svn/../svn-server-sync/joal/trunk@489 03bf7f67-59de-4072-a415-9a990d468a3f

--- make/joal-alc-CustomJavaCode.java
+++ make/joal-alc-CustomJavaCode.java
@@ -4,3 +4,7 @@ public java.lang.String alcGetString(ALCdevice device, int param);
 /** Fetches the names of the available ALC device specifiers.
     Equivalent to the C call alcGetString(NULL, ALC_DEVICE_SPECIFIER). */
 public java.lang.String[] alcGetDeviceSpecifiers();
+
+/** Fetches the names of the available ALC capture device specifiers.
+    Equivalent to the C call alcGetString(NULL, ALC_CAPTURE_DEVICE_SPECIFIER). */
+public java.lang.String[] alcGetCaptureDeviceSpecifiers();
--- make/joal-alc-impl-CustomJavaCode.java
+++ make/joal-alc-impl-CustomJavaCode.java
@@ -19,7 +19,17 @@ public java.lang.String alcGetString(ALCdevice device, int param) {
 /** Fetches the names of the available ALC device specifiers.
     Equivalent to the C call alcGetString(NULL, ALC_DEVICE_SPECIFIER). */
 public java.lang.String[] alcGetDeviceSpecifiers() {
-  ByteBuffer buf = alcGetStringImpl(null, ALC_DEVICE_SPECIFIER);
+  return getDoubleNullTerminatedString(ALC_DEVICE_SPECIFIER);
+}
+
+/** Fetches the names of the available ALC capture device specifiers.
+    Equivalent to the C call alcGetString(NULL, ALC_CAPTURE_DEVICE_SPECIFIER). */
+public java.lang.String[] alcGetCaptureDeviceSpecifiers() {
+  return getDoubleNullTerminatedString(ALC_CAPTURE_DEVICE_SPECIFIER);
+}
+
+private java.lang.String[] getDoubleNullTerminatedString(int which) {
+  ByteBuffer buf = alcGetStringImpl(null, which);
   if (buf == null) {
     return null;
   }
