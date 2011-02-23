ec507c11ef64de269248b7b9dafceece7f08f56b
Sven Gothel <sgothel@jausoft.com>
2011-02-23 15:13:43 +0100

    Inverse build attribute build.noarchive -> build.archiveon

--- make/scripts/make.joal.all.win32.bat
+++ make/scripts/make.joal.all.win32.bat
@@ -7,6 +7,5 @@ set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw\bin;%PATH%
 set LIB_GEN=%THISDIR%\lib
 set CLASSPATH=.;%THISDIR%\build-win32\classes
 REM    -Dc.compiler.debug=true 
-REM    -Dbuild.noarchives=true
 
-ant -Dbuild.noarchives=true -Drootrel.build=build-win32 %1 %2 %3 %4 %5 %6 %7 %8 %9 > make.joal.all.win32.log 2>&1
+ant -Drootrel.build=build-win32 %1 %2 %3 %4 %5 %6 %7 %8 %9 > make.joal.all.win32.log 2>&1
--- make/scripts/make.joal.all.win64.bat
+++ make/scripts/make.joal.all.win64.bat
@@ -7,6 +7,5 @@ set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw64\bin;c:\mingw\bin;%PATH%
 set LIB_GEN=%THISDIR%\lib
 set CLASSPATH=.;%THISDIR%\build-win64\classes
 REM    -Dc.compiler.debug=true 
-REM    -Dbuild.noarchives=true
 
-ant -Dc.compiler.debug=true -Dbuild.noarchives=true -Drootrel.build=build-win64 %1 %2 %3 %4 %5 %6 %7 %8 %9 > make.joal.all.win64.log 2>&1
+ant -Dc.compiler.debug=true -Drootrel.build=build-win64 %1 %2 %3 %4 %5 %6 %7 %8 %9 > make.joal.all.win64.log 2>&1
