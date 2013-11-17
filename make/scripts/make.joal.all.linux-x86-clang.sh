ada9e6c5852d279e4cae96e3ec9a15e5419ff271
Sven Gothel <sgothel@jausoft.com>
2013-11-17 17:02:31 +0100

    Use 'gluegen-clang.properties' for generic clang and 'gluegen-xcode_clang.properties' for OSX xcode-clang ; Add GNU/Linux LLVM/clang build scripts

---
+++ make/scripts/make.joal.all.linux-x86-clang.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+SDIR=`dirname $0` 
+
+if [ -e $SDIR/../../../gluegen/make/scripts/setenv-build-jogl-x86.sh ] ; then
+    . $SDIR/../../../gluegen/make/scripts/setenv-build-jogl-x86.sh
+fi
+
+if [ -z "$ANT_PATH" ] ; then
+    if [ -e /usr/share/ant/bin/ant -a -e /usr/share/ant/lib/ant.jar ] ; then
+        ANT_PATH=/usr/share/ant
+        export ANT_PATH
+        echo autosetting ANT_PATH to $ANT_PATH
+    fi
+fi
+if [ -z "$ANT_PATH" ] ; then
+    echo ANT_PATH does not exist, set it
+    exit
+fi
+
+export SOURCE_LEVEL=1.6
+export TARGET_LEVEL=1.6
+export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
+
+export GLUEGEN_PROPERTIES_FILE="lib/gluegen-clang.properties"
+# or -Dgcc.compat.compiler=clang
+
+#export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
+export JOGAMP_JAR_CODEBASE="Codebase: *.goethel.localnet"
+
+ant  \
+    -Drootrel.build=build-x86-clang \
+    $* 2>&1 | tee make.joal.all.linux-x86-clang.log
