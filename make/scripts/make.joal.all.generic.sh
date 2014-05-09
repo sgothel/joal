38277fff31277fe67024db9c2e363d9244863d17
Sven Gothel <sgothel@jausoft.com>
2014-05-09 09:45:58 +0200

    Add generic manual build script to test against distribution's default openjdk + ant

---
+++ make/scripts/make.joal.all.generic.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+export SOURCE_LEVEL=1.6
+export TARGET_LEVEL=1.6
+export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
+
+#export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
+export JOGAMP_JAR_CODEBASE="Codebase: *.goethel.localnet"
+
+#BUILD_ARCHIVE=true \
+ant  \
+    $* 2>&1 | tee make.joal.all.generic.log
