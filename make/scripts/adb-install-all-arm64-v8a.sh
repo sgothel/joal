232aee061552a4091fce7368ea8bc9bba8e404ff
Sven Gothel <sgothel@jausoft.com>
2015-01-30 07:17:48 +0100

    Bug 1122: Add AArch64 support (Android, GNU/Linux and in general)

---
+++ make/scripts/adb-install-all-arm64-v8a.sh
@@ -0,0 +1,4 @@
+#adb $* install ../build-android-aarch64/jogamp-android-launcher.apk
+#adb $* install ../build-android-aarch64/gluegen-rt-android-arm64-v8a.apk
+adb $* install ../build-android-aarch64/jar/joal-android-arm64-v8a.apk
+adb $* install ../build-android-aarch64/jar/joal-test-android.apk
