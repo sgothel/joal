a997ae58e7928f27fa618d9c06c7ffb2eb879709
Sven Gothel <sgothel@jausoft.com>
2020-01-02 00:31:48 +0100

    Add iOS build scripts

---
+++ make/scripts/make.joal.all.ios.arm64.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+if [ -e /usr/local/etc/profile.ant ] ; then
+    . /usr/local/etc/profile.ant
+fi
+
+#    -Dc.compiler.debug=true 
+#
+#    -Dtarget.sourcelevel=1.6 \
+#    -Dtarget.targetlevel=1.6 \
+#    -Dtarget.rt.jar=/opt-share/jre1.6.0_30/lib/rt.jar \
+
+# Force OSX SDK 10.6, if desired
+# export SDKROOT=macosx10.6
+
+export SDKROOT=iphoneos13.2
+xcrun --show-sdk-path
+
+JAVA_HOME=`/usr/libexec/java_home -version 11`
+PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME PATH
+which java
+java -version 
+
+export SOURCE_LEVEL=1.8
+export TARGET_LEVEL=1.8
+export TARGET_RT_JAR=/usr/local/jre1.8.0_212/lib/rt.jar
+
+export GLUEGEN_CPPTASKS_FILE=make/lib/gluegen-cpptasks-ios-aarch64.xml
+
+#export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
+export JOGAMP_JAR_CODEBASE="Codebase: *.goethel.localnet"
+
+ant \
+    -Drootrel.build=build-ios-arm64 \
+    $* 2>&1 | tee make.joal.all.ios-arm64.log
