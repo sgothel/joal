30368d69ec33f006b901c6888ce9876c12b0e6af
Sven Gothel <sgothel@jausoft.com>
2023-01-15 04:08:38 +0100

    Build scripts: MacOS + Windows: Use JDK-17 to build our Java-1.8 API

--- make/scripts/make.joal.all.macosx.sh
+++ make/scripts/make.joal.all.macosx.sh
@@ -4,7 +4,7 @@ if [ -e /usr/local/etc/profile.ant ] ; then
     . /usr/local/etc/profile.ant
 fi
 
-JAVA_HOME=`/usr/libexec/java_home -version 11`
+JAVA_HOME=`/usr/libexec/java_home -version 17`
 PATH=$JAVA_HOME/bin:$PATH
 export JAVA_HOME PATH
 
