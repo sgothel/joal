a1312ed74545eff208ed560a0989523a89dde3e9
Sven Gothel <sgothel@jausoft.com>
2023-07-09 14:51:44 +0200

    ALExtImpl: Remove unused imports

--- src/java/jogamp/openal/ALExtImpl.java
+++ src/java/jogamp/openal/ALExtImpl.java
@@ -1,12 +1,5 @@
 package jogamp.openal;
 
-import com.jogamp.common.nio.Buffers;
-import com.jogamp.openal.ALException;
-import com.jogamp.openal.ALCdevice;
-import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-
 /**
  * ALExt implementation.
  */
