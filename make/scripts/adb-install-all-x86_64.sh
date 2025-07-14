17ec23123afd4ef1472fdd887b0441e4479304af
Sven Göthel <sgothel@jausoft.com>
2025-07-14 03:06:09 +0200

    Bug 1520: Add `android:exported="true"` to activities + ...
    + update/fix adb-install* scripts
    - Eclipse: Use android-stripped.jar in .classpath (via use library)

---
+++ make/scripts/adb-install-all-x86_64.sh
@@ -0,0 +1,4 @@
+#adb $* install ../../gluegen/build-android-x86_64/jogamp-android-launcher.apk
+#adb $* install ../../gluegen/build-android-x86_64/gluegen-rt-android-x86_64.apk
+adb $* install ../build-android-x86_64/jar/joal-android-x86_64.apk
+adb $* install ../build-android-x86_64/jar/joal-test-android.apk
